e0fd8416306eada27cd7dd86b4c90ce041ea6de6
Marco G. Maia <marcogmaia@gmail.com>
2021-04-20 06:55:01 -0300

    feat: rng random choice

--- src/utils/rng.hpp
+++ src/utils/rng.hpp
@@ -7,6 +7,10 @@ namespace radl::rng {
 
 
 class random_number_generator {
+private:
+    int g_seed;
+    std::mt19937_64 rng;
+
 public:
     random_number_generator();
     explicit random_number_generator(const int seed);
@@ -14,11 +18,29 @@ public:
 
     int roll_dice(const int& n, const int& d);
     int range(const int& min, const int& max);
-    uint_fast64_t initial_seed;
 
-private:
-    int g_seed;
-    std::mt19937_64 rng;
+    /**
+     * @brief Picks a random value in range [first - last] and puts in "output"
+     *
+     * @tparam InIter
+     * @tparam OutIter
+     * @param first
+     * @param last
+     * @param output
+     * @return true
+     * @return false
+     */
+    template <typename InIter, typename OutIter>
+    bool random_choice(InIter first, InIter last, OutIter output) {
+        // if empty
+        if(first == last) {
+            return false;
+        }
+        std::sample(first, last, output, 1, this->rng);
+        return true;
+    }
+
+    uint_fast64_t initial_seed;
 };
 
 extern random_number_generator rng;
